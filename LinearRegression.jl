b72eebd63397d0e540ad283287fa81209f8985a1
Stefan Volz <volzstefan97@googlemail.com>
2019-12-21 19:05:35 +0100

    split linear regression and tests into seperate files

--- LinearRegression.jl
+++ LinearRegression.jl
@@ -1,8 +1,10 @@
-import Random
-Random.seed!(0)
+module LinearRegression
 
-using Plots
+import Random
+
 using LinearAlgebra
+
+export fit_linear_model
 
 # clear all files
 open("norm_gradient_w.txt", "w") do io
@@ -34,16 +36,18 @@ end
     Φ(k, 𝐱ₙ): Basis function
     𝐗: Set of inputs 𝐱ₙ where 𝐱ₙ is an input vector to Φ
     t: corresponding target values for each 𝐱ₙ
-    k: Index for 𝐰ₖ in respect to which the derivative is taken
     𝐰: Parameters
+    k: Index for 𝐰ₖ in respect to which the derivative is taken
+    ω: Weight decay factor
 """
 function ∂E_D∂w_k(Φ::Function,
   𝐗::Matrix{<:Number},
   t::Vector{<:Number},
   𝐰::Vector{<:Number},
-  k::Integer)::Number
+  k::Integer,
+  ω::Real)::Number
     N = size(t)[1]
-    - Σ(1, N, n->Φ(k, 𝐗[n, :]) * (t[n] - y(𝐰, Φ, 𝐗[n,:])))
+    - Σ(1, N, n->Φ(k, 𝐗[n, :]) * (t[n] - y(𝐰, Φ, 𝐗[n,:])) - ω*𝐰[k])
 end
 
 """Error function
@@ -52,13 +56,15 @@ end
     𝐗: Set of inputs 𝐱ₙ where 𝐱ₙ is an input vector to Φ
     t: corresponding target values for each 𝐱ₙ
     𝐰: Parameters
+    ω: Weight decay factor
 """
 function E_D(Φ::Function,
   𝐗::Matrix{<:Number},
   t::Vector{<:Number},
-  𝐰::Vector{<:Number})::Number
+  𝐰::Vector{<:Number},
+  ω::Real)::Number
     N = size(t)[1]
-    1 // 2 * Σ(1, N, n->(t[n] - y(𝐰, Φ, 𝐗[n,:]))^2)
+    1 // 2 * Σ(1, N, n->(t[n] - y(𝐰, Φ, 𝐗[n,:]))^2 + ω*norm(𝐰)^2)
 end
 
 """One iteration of the gradient descent algorithm
@@ -72,6 +78,7 @@ end
     η: Learning rate
     ∇𝐰_prior: Gradient of parameters from prior iteration
     γ: Momentum factor
+    ω: Weight decay factor
 """
 function gradient_descent_iteration(∂E_D∂w_k::Function,
   𝐗::Matrix{<:Number},
@@ -79,11 +86,12 @@ function gradient_descent_iteration(∂E_D∂w_k::Function,
   𝐰::Vector{<:Number},
   η::Number,
   ∇𝐰_prior::Vector{<:Number},
-  γ::Number)::Tuple{Vector{<:Number},Vector{<:Number}}
+  γ::Number,
+  ω::Real)::Tuple{Vector{<:Number},Vector{<:Number}}
     M = size(𝐰)[1]
     ∇𝐰 = γ * ∇𝐰_prior
     for j = 1:M
-        ∂E_D∂w_jk(k) = ∂E_D∂w_k(𝐗, t, 𝐰, k)
+        ∂E_D∂w_jk(k) = ∂E_D∂w_k(𝐗, t, 𝐰, k, ω)
         ∇𝐰 += collect(map(∂E_D∂w_jk, 1:M))
 
         open("norm_gradient_w.txt", "a") do io
@@ -107,6 +115,7 @@ end
     ε: Gradient descent stops once the difference between two iterations
         (𝐰 and 𝐰') is less than ε
     γ: Momentum factor
+    ω: Weight decay factor
 
 # Fails:
     Fails on encountering NaN in computation or on Divergence to Inf
@@ -119,19 +128,20 @@ function gradient_descent(∂E_D∂w_k::Function,
   iters::Integer,
   𝐰::Vector{<:Number},
   ε = 10e-12::Number,
-  γ = 0.9::Real)::Tuple{Vector{<:Number},Integer}
+  γ = 0.9::Real,
+  ω = 0.01::Real)::Tuple{Vector{<:Number},Integer}
     ∇𝐰 = zero(𝐰)
     did_iters = 0
     for i = 1:iters
         did_iters += 1
         𝐰_old = 𝐰
         open("error.txt", "a") do io
-            write(io, string(E_D(Φ, 𝐗, t, 𝐰)), "\n")
+            write(io, string(E_D(Φ, 𝐗, t, 𝐰, ω)), "\n")
         end
         open("learning_rate.txt", "a") do io
             write(io, string(η), "\n")
         end
-        (𝐰, ∇𝐰) = gradient_descent_iteration(∂E_D∂w_k, 𝐗, t, 𝐰, η, ∇𝐰, γ)
+        (𝐰, ∇𝐰) = gradient_descent_iteration(∂E_D∂w_k, 𝐗, t, 𝐰, η, ∇𝐰, γ, ω)
          if any(isnan.(𝐰))
            error("Encountered NaN") 
         end
@@ -156,6 +166,7 @@ end
     ε: Gradient descent stops once the difference between
         two iterations (𝐰 and 𝐰') is less than ε
     γ: Momentum Parameter
+    ω: Weight decay factor
     optimizer: Parameter to select optimizer that's used
 
 # Fails:
@@ -169,11 +180,12 @@ function fit_linear_model(Φ::Function,
   iters::Integer,
   ε = 10e-12::Number,
   γ = 0.9::Real,
+  ω = 0.01::Real,
   optimizer = :gradient_descent)::Tuple{Function,Number}
     if optimizer == :gradient_descent
-        (𝐰, did_iters) = gradient_descent((𝐗, t, 𝐰, k)->∂E_D∂w_k(Φ, 𝐗, t, 𝐰, k),
-            𝐗, t, η, M, iters, randn(M), ε, γ)
-        residual_error = E_D(Φ, 𝐗, t, 𝐰)
+        (𝐰, did_iters) = gradient_descent((𝐗, t, 𝐰, k, ω)->∂E_D∂w_k(Φ, 𝐗, t, 𝐰, k, ω),
+            𝐗, t, η, M, iters, randn(M), ε, γ, ω)
+        residual_error = E_D(Φ, 𝐗, t, 𝐰, ω)
         println(𝐰, " after ", did_iters, " iterations. Residual error: ", residual_error)
         (𝐱->y(𝐰, Φ, 𝐱), residual_error)
     else
@@ -181,53 +193,6 @@ function fit_linear_model(Φ::Function,
     end
 end
 
-# optimal solution using a linear model for comparison
+Φ = (j, 𝐱) -> 𝐱[1]^j
 
-x_b = [1, 2, 3]
-y_b = [1, 1, 2]
-
-function fitline(x::Vector{<:Number}, y::Vector{<:Number})::Function
-    n = size(x)[1]
-    a = (sum(y) * sum(x.^2) - sum(x) * sum(x .* y)) / (n * sum(x.^2) - sum(x)^2) # t
-    b = (n * sum(x .* y) - sum(x) * sum(y)) / (n * sum(x.^2) - sum(x)^2)# m
-    x->b * x + a
 end
-
-optimal_linear_model = fitline(x_b, y_b)
-
-# end of optimal solution
-
-# a few different basis functions for testing
-Φ1(j, 𝐱) =
-    if j == 0
-        1
-    else
-        𝐱[1]
-end
-
-Φ2(j, 𝐱) = 𝐱[1]^j
-Φ3(j, 𝐱) = sin(1 / j * 𝐱[1])
-σ(a) = 1 / (1 + exp(-a))
-function Φ4(j, 𝐱)
-    μ = 0.2
-    s = 0.2
-    σ((𝐱[1] - μ) / s)
-end
-
-# test3
-𝐗 = hcat([0; 1; 2; 3; 4; 5]) # hcat to convert to matrix because julia is weird like that
-t = [0, 1, 4, 9, 16, 25]
-t += randn(size(t)[1]) * 3
-
-
-Φ = Φ2
-
-(model1, residual_error) = fit_linear_model(Φ2, 𝐗, t, 0.00005, 2, 300000, 10e-3, 0.5) # Φ2, 𝐗, t, 0.00005, 2, 300000, 10e-5, 0.2)
-
-
-x = 0:0.1:5
-
-p = scatter(map(x->x[1], 𝐗), t, label = "training");
-plot!(x, model1.([[x] for x in x]), label = "prediction")
-display(p)
-readline()
--- Tests.jl
+++ Tests.jl
@@ -1,3 +1,44 @@
+# run `export JULIA_LOAD_PATH=.:` to enable loading of LinearRegression module
+
+using Plots
+using LinearRegression
+
+import Random
+
+Random.seed!(0)
+
+# optimal solution using a linear model for comparison
+
+x_b = [1, 2, 3]
+y_b = [1, 1, 2]
+
+function fitline(x::Vector{<:Number}, y::Vector{<:Number})::Function
+    n = size(x)[1]
+    a = (sum(y) * sum(x.^2) - sum(x) * sum(x .* y)) / (n * sum(x.^2) - sum(x)^2) # t
+    b = (n * sum(x .* y) - sum(x) * sum(y)) / (n * sum(x.^2) - sum(x)^2)# m
+    x->b * x + a
+end
+
+optimal_linear_model = fitline(x_b, y_b)
+
+# end of optimal solution
+
+# a few different basis functions for testing
+Φ1(j, 𝐱) =
+    if j == 0
+        1
+    else
+        𝐱[1]
+end
+
+Φ2(j, 𝐱) = 𝐱[1]^j
+Φ3(j, 𝐱) = sin(1 / j * 𝐱[1])
+σ(a) = 1 / (1 + exp(-a))
+function Φ4(j, 𝐱)
+    μ = 0.2
+    s = 0.2
+    σ((𝐱[1] - μ) / s)
+end
 
 """
 # test1
@@ -32,3 +73,38 @@ plot!(x, model1.(map(x->[x], x)), label = "prediction")
 display(p)
 readline()
 """
+
+"""
+# test3
+𝐗 = hcat([0; 1; 2; 3; 4; 5]) # hcat to convert to matrix because julia is weird like that
+t = [0, 1, 4, 9, 16, 25]
+t += randn(size(t)[1]) * 3
+
+(model1, residual_error) = fit_linear_model(Φ2, 𝐗, t, 0.00005, 2, 300000, 10e-12, 0.9) # Φ2, 𝐗, t, 0.00005, 2, 300000, 10e-5, 0.2)
+
+x = 0:0.1:5
+
+p = scatter(map(x->x[1], 𝐗), t, label = "training");
+plot!(x, model1.([[x] for x in x]), label = "prediction")
+display(p)
+readline()
+"""
+# test4
+using StatsBase
+
+f(x) = sin(3*x + 10)
+x = -1:0.1:2
+y = f.(x)
+𝐗 = hcat([x for x in sample(x, 20)]) # hcat to convert to matrix because julia is weird like that
+t = f.(𝐗)[:]
+t += randn(size(t)[1]) * 0.3
+
+(model1, residual_error) = fit_linear_model(Φ2, 𝐗, t, 0.00008, 5, 200000, 10e-8, 0.95, 0.008)
+
+x = -1:0.1:2
+
+p = scatter(map(x->x[1], 𝐗), t, label = "training");
+plot!(x, model1.([[x] for x in x]), label = "prediction")
+plot!(x, y, label = "real")
+display(p)
+readline()
